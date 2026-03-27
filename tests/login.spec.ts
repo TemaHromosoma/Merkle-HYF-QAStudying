eb10fc7ebb4e7e2d902ac6f5993865019d75eac1
TemaHromosoma <yakovishinartem@gmail.com>
2026-03-27 12:17:22 +0100

    deleted beforeEach in login.spec, deleted unused page parameters inside navigation tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -4,15 +4,6 @@ import { test, expect } from '../fixtures/test-fixtures';
 
 test.describe('Basic login functionality tests', () => {
 
-    // let loginPage: LoginPage;
-
-    // test.beforeEach(async ({ page }) => {
-
-    //     loginPage = new LoginPage(page);
-
-    //     await loginPage.goto();
-    // });
-
     test('Successful login operation', async ({ loginPage, page }) => {
 
         await loginPage.login('aguspe', '12341234');
@@ -24,7 +15,7 @@ test.describe('Basic login functionality tests', () => {
         await expect(loginPage.welcomeMessage).toHaveText('Welcome to your account dashboard. From here you can manage your orders and account details.')
     });
 
-    test('Failed login operation with invalid password', async ({ loginPage, page }) => {
+    test('Failed login operation with invalid password', async ({ loginPage }) => {
 
         await loginPage.login('aguspe', 'wrongpassword');
 
@@ -33,14 +24,14 @@ test.describe('Basic login functionality tests', () => {
         await expect(loginPage.errorMessage).toHaveText('Error: Incorrect login or password provided.');
     });
 
-    test('Empty username submission', async ({ loginPage, page }) => {
+    test('Empty username submission', async ({ loginPage }) => {
 
         await loginPage.login('', '12341234');
 
         await expect(loginPage.usernameInput).toHaveAttribute('required', '');
     });
 
-    test('Empty password submission', async ({ loginPage, page }) => {
+    test('Empty password submission', async ({ loginPage }) => {
 
         await loginPage.login('aguspe', '');
 
